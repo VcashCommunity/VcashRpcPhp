4e39341ea1d8883eac85925d2a201b2615454b7b
devmahno <lemaxho>
2016-12-24 18:41:32 +0100

    Add test file

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+
+// Test module
+session_start();
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json;charset=utf-8");
+
+/**
+ * Request rpc class and functions
+ */
+require_once 'rpc.php';
+
+echo "rpc_getinfo\n";
+echo json_encode(VcashRpc::rpc_getinfo(), JSON_PRETTY_PRINT);
+echo "\n\nrpc_getbalance\n";
+echo json_encode(VcashRpc::rpc_getbalance(), JSON_PRETTY_PRINT);
+echo "\n\nrpc_getblockcount\n";
+echo json_encode(VcashRpc::rpc_getblockcount(), JSON_PRETTY_PRINT);
